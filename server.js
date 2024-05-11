5c859b2b09363d968eadec49177c14e6a2024935
Rahul Kumar <mrrahulkumarvns@gmail.com>
2024-05-11 08:47:38 +0530

    remove logs

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
-  // await mongoose.connect('mongodb+srv://rahul:SLAvds0ppjoELQjM@cluster0.x82uvpt.mongodb.net/EcommerceDatabase',{ useNewUrlParser: true,useUnifiedTopology: true,});
-
   console.log("db connected");
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 //enabeling cors
